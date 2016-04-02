62e387b1e7f70bf4d76baf0cbef1c40f50733b21
Fernando Gomez <fernando.gmz12@gmail.com>
2016-04-02 18:27:25 -0300

    Funciones 1 a 12

--- recursosdecatedra/Tp.hs
+++ recursosdecatedra/Tp.hs
@@ -21,9 +21,7 @@ mean xs = realToFrac (sum xs) / genericLength xs
 
 -- Punto 1
 split :: Eq a => a -> [a] -> [[a]]
-split delimiter = foldr f [[]] 
-          where f c l@(x:xs) | c == delimiter = []:l
-                             | otherwise = (c:x):xs
+split del = foldr ( \x (ys:rec) -> if x == del then []:(ys:rec) else (x:ys):rec ) [[]]
 
 -- Punto 2
 longitudPromedioPalabras :: Extractor
@@ -33,38 +31,95 @@ longitudPromedioPalabras frase =  mean (map genericLength (split ' ' frase))
 cuentas :: Eq a => [a] -> [(Int, a)]
 cuentas lista = nub (map (\x -> (contar (==x) lista, x)) lista)
 
-contar a = length . filter a
+contar :: Eq a => (a  -> Bool) -> [a] -> Int
+contar a = genericLength . filter a
 
 -- Punto 4
 repeticionesPromedio :: Extractor
-repeticionesPromedio = undefined
+repeticionesPromedio texto = mean ( (map (\(x, y) -> fromIntegral x) (cuentas (split ' ' texto)) ) )
 
+-- Punto 5
 tokens :: [Char]
 tokens = "_,)(*;-=>/.{}\"&:+#[]<|%!\'@?~^$` abcdefghijklmnopqrstuvwxyz0123456789"
 
 frecuenciaTokens :: [Extractor]
-frecuenciaTokens = undefined
+frecuenciaTokens = [ \texto -> fromIntegral(contar (==tok) texto) / genericLength texto | tok <- tokens ]
 
+-- Punto 6
 normalizarExtractor :: [Texto] -> Extractor -> Extractor
-normalizarExtractor = undefined
+normalizarExtractor txts extractor = (\texto -> (extractor  texto) / norma) where norma = maximum (map (abs . extractor) txts)
 
+-- Punto 7
 extraerFeatures :: [Extractor] -> [Texto] -> Datos
-extraerFeatures = undefined
+extraerFeatures exs txts = [ [extractor  txt | extractor <- [normalizarExtractor txts ex | ex <- exs ]] | txt <- txts ]
 
+-- Punto 8
 distEuclideana :: Medida
-distEuclideana = undefined
+distEuclideana p q = sqrt (sum (map (\x -> x*x) (zipWith (-) p q)))
 
 distCoseno :: Medida
-distCoseno = undefined
-
+distCoseno p q = (prodVect p q) / (norma p * norma q)
+                  where prodVect p q = sum(zipWith (*) p q)
+                        norma r = sqrt(prodVect r r)
+-- Punto 9
 knn :: Int -> Datos -> [Etiqueta] -> Medida -> Modelo
-knn = undefined
+knn k datos etiquetas medida =  (\nuevoDato -> snd (maximo (cuentas (getk k (ordenar (zipWith (\x y -> (x,y)) (map (medida nuevoDato) datos) etiquetas))))))
 
-accuracy :: [Etiqueta] -> [Etiqueta] -> Float
-accuracy = undefined
+orden x z | fst x < fst z = LT
+          |otherwise = GT
+ordenar xs = sortBy orden xs
+ 
+getk k lista = map (\x -> snd x) (take k lista)
 
+ordenEtiquetas x z | fst x < fst z = LT
+                   |otherwise = GT
+
+maximo = maximumBy ordenEtiquetas 
+
+-- Punto 9 (Solución de Javier)
+--knn :: Int -> Datos -> [Etiqueta] -> Medida -> Modelo
+--knn cantVecinos datos etiquetas medida instancia = obtenerModa ( vecinosMasCercanos cantVecinos ( etiquetar (obtenerMedidas instancia medida datos) etiquetas)  )
+
+--obtenerMedidas :: Instancia ->Medida ->Datos ->[(Float, Instancia)]
+--obtenerMedidas instanciaOrigen medida datos = [ (medida instanciaOrigen instancia, instancia) | instancia <- datos ]
+
+--etiquetar :: [(Float, Instancia)] -> [Etiqueta] -> [(Float, Etiqueta)]
+--etiquetar medidas etiquetas = [ ( fst ( medidas !! i ), etiquetas !! i ) | i <- [0..(genericLength etiquetas -1)] ]
+
+--vecinosMasCercanos :: Int ->[(Float, Etiqueta)] -> [Etiqueta]
+--vecinosMasCercanos n medidas = map (\x -> snd x) (take n ( sortBy (compararDatos) medidas ))
+
+--compararDatos :: (Float, Etiqueta) ->(Float, Etiqueta) ->Ordering
+--compararDatos (x1,x2) (y1,y2) = if x1 < y1 then LT else GT
+
+--obtenerModa :: [Etiqueta] ->Etiqueta
+--obtenerModa etiquetas = snd ( maximumBy (compararFrecuencias) (cuentas etiquetas) )
+
+--compararFrecuencias :: (Int, Etiqueta) ->(Int, Etiqueta) ->Ordering
+--compararFrecuencias (x1,x2) (y1,y2) = if x1 < y1 then LT else GT
+
+
+
+-- Punto 10
 separarDatos :: Datos -> [Etiqueta] -> Int -> Int -> (Datos, Datos, [Etiqueta], [Etiqueta])
-separarDatos = undefined
+separarDatos datos etiquetas n p = ([ datos !! j | j <- [0..(n * tamPart - 1)], notElem j [((p-1) * tamPart)..(p * tamPart - 1)] ],
+                                    [ datos !! i | i<-[((p-1) * tamPart)..(p * tamPart - 1)] ],
+                                    [ etiquetas !! j | j <- [0..(n * tamPart - 1)], notElem j [((p-1) * tamPart)..(p * tamPart - 1)] ],
+                                    [ etiquetas !! i | i<-[((p-1) * tamPart)..(p * tamPart - 1)] ])
+                                    where tamPart = div (genericLength datos) n
 
+-- Punto 11
+accuracy :: [Etiqueta] -> [Etiqueta] -> Float
+accuracy = \xs ys -> sum(zipWith f xs ys) / fromIntegral(genericLength xs)
+            where f = \x y -> if x == y then 1 else 0 
+
+-- Punto 12
 nFoldCrossValidation :: Int -> Datos -> [Etiqueta] -> Float
-nFoldCrossValidation = undefined
+nFoldCrossValidation n datos etiquetas = mean [fold n datos etiquetas i  | i <- [0..14]] 
+
+fold n datos etiquetas i = accuracy (map (knn 15 entrenamiento etiquetasEnt distEuclideana) test) etiquetasTest
+  where particion = separarDatos datos etiquetas n i
+  entrenamiento = fst particion
+  test = snd(fst(particion))
+  etiquetasEnt = snd(snd(fst(particion)))
+  etiquetasTest = snd(snd(snd(particion)))
